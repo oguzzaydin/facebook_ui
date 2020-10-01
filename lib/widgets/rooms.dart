1633ae637c6e3a108113e73fa675f09de1877753
Oğuzhan Aydın <oguzhan.aydin@medyasoft.com.tr>
2020-10-02 01:26:04 +0300

    post rooms widgets

---
+++ lib/widgets/rooms.dart
@@ -0,0 +1,81 @@
+import 'package:facebook_ui/config/palette.dart';
+import 'package:facebook_ui/models/models.dart';
+import 'package:facebook_ui/widgets/widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Rooms extends StatelessWidget {
+  final List<User> onlineUsers;
+
+  const Rooms({
+    Key key,
+    @required this.onlineUsers,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60.0,
+      color: Colors.white,
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 4.0,
+        ),
+        scrollDirection: Axis.horizontal,
+        itemCount: onlineUsers.length + 1,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: _CreateRoomWButton(),
+            );
+          }
+          final User user = onlineUsers[index - 1];
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ProfileAvatar(
+              imageUrl: user.imageUrl,
+              isActive: true,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _CreateRoomWButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      onPressed: () => print('Create room'),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      color: Colors.white,
+      borderSide: BorderSide(
+        width: 3.0,
+        color: Colors.blueAccent[100],
+      ),
+      textColor: Palette.facebookBlue,
+      child: Row(
+        children: [
+          ShaderMask(
+            shaderCallback: (Rect bounds) =>
+                Palette.createRoomGradient.createShader(bounds),
+            child: Icon(
+              Icons.video_call,
+              size: 35.0,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            width: 4.0,
+          ),
+          Text('Create\nroom')
+        ],
+      ),
+    );
+  }
+}
